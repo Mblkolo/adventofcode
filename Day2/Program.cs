d45294848764207bba03001a400204c51ee4955c
Mblkolo <rusakov-rv@yandex.ru>
2015-12-18 17:12:46 +0500

    Добавил тестровые данные по второму дню

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Day2
     {
         static void Main(string[] args)
         {
+            string[] allLines = File.ReadAllLines("inputTest.txt");
+            Console.WriteLine(allLines);
         }
     }
 }
