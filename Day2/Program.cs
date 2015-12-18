36409c75441b07f0d1611a790935a4eeb5e6e271
Mblkolo <rusakov-rv@yandex.ru>
2015-12-18 17:25:12 +0500

    Решил задачу 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -11,8 +11,26 @@ namespace Day2
     {
         static void Main(string[] args)
         {
-            string[] allLines = File.ReadAllLines("inputTest.txt");
-            Console.WriteLine(allLines);
+            string[] allLines = File.ReadAllLines("input.txt");
+
+            int result = 0;
+            foreach (var line in allLines)
+            {
+                var sizes = line.Split('x').Select(x => int.Parse(x)).ToArray();
+                var a = sizes[0] * sizes[1];
+                var b = sizes[1] * sizes[2];
+                var c = sizes[2] * sizes[0];
+
+                var min = a < b ? a : b;
+                min = min < c ? min : c;
+
+                var size = min + 2 * a + 2 * b + 2 * c;
+                result += size;
+            }
+
+            Console.WriteLine(result);
+
+            Console.Read();
         }
     }
 }
