cd59f8b314362d24011d9a9fb2a8e251d4f3560f
Mblkolo <rusakov-rv@yandex.ru>
2015-12-18 17:37:12 +0500

    Первый день на повышенной сложности

---
+++ Day1Hard/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("input.txt");
+            string input = allLines[0];
+
+            var bases = 0;
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (input[i] == '(')
+                    bases++;
+
+                if (input[i] == ')')
+                    bases--;
+
+                if (bases == -1)
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
