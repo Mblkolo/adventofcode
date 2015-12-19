c9ed0e96ef7f7ce035e0852f7913a9adee63553a
Mblikolo <rusakov-rv@yandex.ru>
2015-12-19 22:14:57 +0500

    Бьюсь со сложным 19 днём

--- Day19Hard/Program.cs
+++ Day19Hard/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day19
+namespace Day19Hard
 {
     class Program
     {
@@ -56,15 +56,45 @@ namespace Day19
                 Tuple.Create("e", "O6"),
             };
 
-            string bel = "C355583BP467838367835F744B82FY5F755825PB828255P43PB823F77558258283675P4BP3F782583F7B5835P3F7P6Y5F75P444BPB825P4BPB83F7BPB835F7BP383F7383BF75F7555828255PBP443F75P4B817PB555558367582F725825835FY583FYF7F7583FYF7583BP6782P3F7583F74383FYF7583BF7583467825825F7P3F783F74444B558355FYF7825P4BP4B58283675F";
+            string seed = "C355583BP467838367835F744B82FY5F755825PB828255P43PB823F77558258283675P4BP3F782583F7B5835P3F7P6Y5F75P444BPB825P4BPB83F7BPB835F7BP383F7383BF75F7555828255PBP443F75P4B817PB555558367582F725825835FY583FYF7F7583FYF7583BP6782P3F7583F74383FYF7583BF7583467825825F7P3F783F74444B558355FYF7825P4BP4B58283675F";
+            string bel = "e";
 
-            var res = new List<string>();
-            foreach(var d in data)
+
+            var front = new Stack<string>();
+            front.Push(seed);
+
+            HashSet<string> processed = new HashSet<string>();
+
+            for(int i=0; front.Count > 0; ++i)
             {
-                res.AddRange(Replase(bel, d.Item1, d.Item2));
+                //var newFront = new List<string>();
+
+                var candidat = front.Pop();
+                var res = new List<string>();
+                foreach (var d in data)
+                {
+                    //res.AddRange(Replase(candidat, d.Item2, d.Item1));
+                    var r = candidat.Replace(d.Item2, d.Item1);
+
+                    if (r != candidat && processed.Add(r))
+                        res.Add(r);
+                }
+
+                if(res.Contains(bel))
+                {
+                    Console.WriteLine(i);
+                    Console.Read();
+                    return;
+                }
+
+
+                foreach( var f in res)
+                    front.Push(f);
+
+                if(i%10000 == 0)
+                    Console.WriteLine("step " + i + " front " + front.Count);
             }
 
-            Console.WriteLine(res.Distinct().Count());
             Console.Read();
         }
 
@@ -73,8 +103,21 @@ namespace Day19
             var res = new List<string>();
             for(int i=0; i<bel.Length; ++i)
             {
-                if (bel[i] == templ[0])
-                    res.Add(bel.Substring(0, i) + repl + bel.Substring(i + templ.Length));
+                if (bel[i] != templ[0])
+                    continue;
+
+                bool isRight = true;
+                for (int t = 0; t < templ.Length; t++ )
+                    if (bel[i + t] != templ[t])
+                    {
+                        isRight = false;
+                        break;
+                    }
+
+                if (!isRight)
+                    continue;
+                
+                res.Add(bel.Substring(0, i) + repl + bel.Substring(i + templ.Length));
             }
 
             return res.ToArray();
