947c8bcc12701eaf7216bdb1ab43b8d01972c802
Mblikolo <rusakov-rv@yandex.ru>
2015-12-20 01:01:46 +0500

    19 по прежнему не даётся

--- Day19Hard/Program.cs
+++ Day19Hard/Program.cs
@@ -11,53 +11,58 @@ namespace Day19Hard
         static void Main(string[] args)
         {
             var data = new[] {
-                Tuple.Create("1", "2F"),
-                Tuple.Create("1", "23F7"),
-                Tuple.Create("B", "B5"),
-                Tuple.Create("B", "4B"),
-                Tuple.Create("B", "43F7"),
-                Tuple.Create("5", "55"),
-                Tuple.Create("5", "PB"),
-                Tuple.Create("5", "P3F7"),
-                Tuple.Create("5", "83FYF7"),
-                Tuple.Create("5", "8367"),
-                Tuple.Create("5", "82"),
-                Tuple.Create("F", "5F"),
-                Tuple.Create("F", "P6"),
-                Tuple.Create("F", "81"),
-                Tuple.Create("H", "C317"),
-                Tuple.Create("H", "C3FYFYF7"),
-                Tuple.Create("H", "C3FY67"),
-                Tuple.Create("H", "C36YF7"),
-                Tuple.Create("H", "H5"),
-                Tuple.Create("H", "N3FYF7"),
-                Tuple.Create("H", "N367"),
-                Tuple.Create("H", "N2"),
-                Tuple.Create("H", "OB"),
-                Tuple.Create("H", "O3F7"),
-                Tuple.Create("6", "BF"),
-                Tuple.Create("6", "46"),
-                Tuple.Create("N", "C3F7"),
-                Tuple.Create("N", "H8"),
-                Tuple.Create("O", "C3FYF7"),
-                Tuple.Create("O", "C367"),
-                Tuple.Create("O", "HP"),
-                Tuple.Create("O", "N3F7"),
-                Tuple.Create("O", "O4"),
-                Tuple.Create("P", "5P"),
-                Tuple.Create("P", "P4"),
-                Tuple.Create("P", "83F7"),
-                Tuple.Create("8", "58"),
-                Tuple.Create("2", "25"),
-                Tuple.Create("4", "BP"),
-                Tuple.Create("4", "44"),
-                Tuple.Create("e", "HF"),
-                Tuple.Create("e", "N1"),
-                Tuple.Create("e", "O6"),
+                Tuple.Create('1', "2F"),
+                Tuple.Create('1', "23F7"),
+                Tuple.Create('B', "B5"),
+                Tuple.Create('B', "4B"),
+                Tuple.Create('B', "43F7"),
+                Tuple.Create('5', "55"),
+                Tuple.Create('5', "PB"),
+                Tuple.Create('5', "P3F7"),
+                Tuple.Create('5', "83FYF7"),
+                Tuple.Create('5', "8367"),
+                Tuple.Create('5', "82"),
+                Tuple.Create('F', "5F"),
+                Tuple.Create('F', "P6"),
+                Tuple.Create('F', "81"),
+                //Tuple.Create('H', "C317"),
+                Tuple.Create('H', "C3FYFYF7"),
+                Tuple.Create('H', "C3FY67"),
+                //Tuple.Create('H', "C36YF7"),
+                Tuple.Create('H', "H5"),
+                Tuple.Create('H', "N3FYF7"),
+                Tuple.Create('H', "N367"),
+                Tuple.Create('H', "N2"),
+                Tuple.Create('H', "OB"),
+                Tuple.Create('H', "O3F7"),
+                Tuple.Create('6', "BF"),
+                Tuple.Create('6', "46"),
+                Tuple.Create('N', "C3F7"),
+                Tuple.Create('N', "H8"),
+                Tuple.Create('O', "C3FYF7"),
+                //Tuple.Create('O', "C367"),
+                Tuple.Create('O', "HP"),
+                Tuple.Create('O', "N3F7"),
+                Tuple.Create('O', "O4"),
+                Tuple.Create('P', "5P"),
+                Tuple.Create('P', "P4"),
+                Tuple.Create('P', "83F7"),
+                Tuple.Create('8', "58"),
+                Tuple.Create('2', "25"),
+                Tuple.Create('4', "BP"),
+                Tuple.Create('4', "44"),
+                Tuple.Create('e', "HF"),
+                Tuple.Create('e', "N1"),
+                Tuple.Create('e', "O6"),
             };
 
-            string seed = "C355583BP467838367835F744B82FY5F755825PB828255P43PB823F77558258283675P4BP3F782583F7B5835P3F7P6Y5F75P444BPB825P4BPB83F7BPB835F7BP383F7383BF75F7555828255PBP443F75P4B817PB555558367582F725825835FY583FYF7F7583FYF7583BP6782P3F7583F74383FYF7583BF7583467825825F7P3F783F74444B558355FYF7825P4BP4B58283675F";
-            string bel = "e";
+            string bel = "C355583BP467838367835F744B82FY5F755825PB828255P43PB823F77558258283675P4BP3F782583F7B5835P3F7P6Y5F75P444BPB825P4BPB83F7BPB835F7BP383F7383BF75F7555828255PBP443F75P4B817PB555558367582F725825835FY583FYF7F7583FYF7583BP6782P3F7583F74383FYF7583BF7583467825825F7P3F783F74444B558355FYF7825P4BP4B58283675F";
+            string seed = "e";
+
+            var count3 = Check(bel, '3');
+            var count7 = Check(bel, '7');
+            var countC = Check(bel, 'C');
+            var countY = Check(bel, 'Y');
 
 
             var front = new Stack<string>();
@@ -70,26 +75,29 @@ namespace Day19Hard
                 //var newFront = new List<string>();
 
                 var candidat = front.Pop();
-                var res = new List<string>();
+                //var res = new List<string>();
                 foreach (var d in data)
                 {
-                    //res.AddRange(Replase(candidat, d.Item2, d.Item1));
-                    var r = candidat.Replace(d.Item2, d.Item1);
+                    var newString = ForvardReplase(candidat, d.Item1, d.Item2);
+                    if (newString.Contains(bel))
+                    {
+                        Console.WriteLine(i);
+                        Console.Read();
+                        return;
+                    }                   
 
-                    if (r != candidat && processed.Add(r))
-                        res.Add(r);
+                    foreach(var n in newString)
+                    {
+                        if (n.Length > bel.Length ||  count3 < Check(n, '3') || count7 < Check(n, '7') || countC < Check(n, 'C') || countY < Check(n, 'Y'))
+                            continue;
+
+                        if (processed.Add(n))
+                            front.Push(n);
+                    }
                 }
 
-                if(res.Contains(bel))
-                {
-                    Console.WriteLine(i);
-                    Console.Read();
-                    return;
-                }
-
-
-                foreach( var f in res)
-                    front.Push(f);
+                //foreach( var f in res)
+                //    front.Push(f);
 
                 if(i%10000 == 0)
                     Console.WriteLine("step " + i + " front " + front.Count);
@@ -97,6 +105,31 @@ namespace Day19Hard
 
             Console.Read();
         }
+
+        static int Check(string bel, char c)
+        {
+            int count = 0;
+            for (int i = 0; i < bel.Length; ++i)
+                if (bel[i] == c)
+                    count++;
+
+            return count;
+        }
+
+        static string[] ForvardReplase(string bel, char templ, string repl)
+        {
+            var res = new List<string>();
+            for (int i = 0; i < bel.Length; ++i)
+            {
+                if (bel[i] != templ)
+                    continue;
+
+                res.Add(bel.Substring(0, i) + repl + bel.Substring(i + 1));
+            }
+
+            return res.ToArray();
+        }
+
 
         static string[] Replase(string bel, string templ, string repl)
         {
