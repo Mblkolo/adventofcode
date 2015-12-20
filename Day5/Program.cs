516d3351f9917c81d3fe464a4b1868b968f1a761
Mblikolo <rusakov-rv@yandex.ru>
2015-12-20 21:30:54 +0500

    Решил 5-й день

---
+++ Day5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("input.txt");
+
+
+            string vowels = "aeiou";
+
+            int niceCount = 0;
+            foreach(var l in allLines)
+            {
+                var count = l.Where(x => vowels.Contains(x)).Count();
+                if (count < 3)
+                    continue;
+
+                bool isDouble = false;
+                for(int i=0; i<l.Length-1; ++i)
+                    if(l[i] == l[i+1])
+                    {
+                        isDouble = true;
+                        break;
+                    }
+
+                if (!isDouble)
+                    continue;
+
+                if (l.Contains("ab") || l.Contains("cd") || l.Contains("pq") || l.Contains("xy"))
+                    continue;
+
+                niceCount++;
+            }
+
+            Console.WriteLine(niceCount);
+            Console.Read();
+            
+        }
+    }
+}
