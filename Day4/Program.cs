11bb682af6d2a5c7f90b4281344f1c35dc082786
Mblkolo <rusakov-rv@yandex.ru>
2015-12-21 12:40:05 +0500

    Сделал 4-й день

---
+++ Day4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = "bgvyzdsv";
+            string input = "bgvyzdsv";
+
+            MD5 md5Hasher = MD5.Create();
+            
+            for(int g=0; g<10000000; ++g)
+            {
+                string tt = input + g;
+                // Преобразуем входную строку в массив байт и вычисляем хэш
+                byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(tt));
+ 
+                // Создаем новый Stringbuilder (Изменяемую строку) для набора байт
+                StringBuilder sBuilder = new StringBuilder();
+ 
+                // Преобразуем каждый байт хэша в шестнадцатеричную строку
+                for (int i = 0; i < data.Length; i++)
+                {
+                    //указывает, что нужно преобразовать элемент в шестнадцатиричную строку длиной в два символа
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                if(sBuilder.ToString().StartsWith("00000"))
+                {
+                    Console.WriteLine(tt);
+                }
+            }
+
+            Console.WriteLine("finish");
+            Console.Read();
+        }
+    }
+}
